b45ebd384acc7d83d5c6dbd519d4a74a4c46a80c
jefferson-calmon <62778628+jefferson-calmon@users.noreply.github.com>
2020-06-26 00:05:33 -0300

    build: Completing the header in started page

--- src/pages/Started/index.jsx
+++ src/pages/Started/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { FaSearch, FaBell, FaCog, FaPlay } from 'react-icons/fa'
 
 import './styles.css';
 
 
 import Light from './assets/light_bulb.png';
+import profileImage from './assets/profile.png';
 
 
 const Started = () => {
     return(
-        <div className="container">
+        <div className="started">
             <header id="topo">
                 <div className="corp">
                     <div className="image-logo-corporation">
@@ -19,11 +21,64 @@ const Started = () => {
                 </div>
 
                 <div className="fastTools">
-                    <div >
+                    <div className="inputBox">
+                        <div className="input">
+                            <input type="search"/>
+                            <span>Buscar</span>
+                        </div>
+                        <div className="iconSearch">
+                            <FaSearch />
+                        </div>
+                    </div>
 
+                    <div className="config">
+                        <div className="setting">
+                            <FaCog className="settingIcon"/>
+                            <FaBell  className="settingIcon"/>
+                        </div>
+
+                        <div className="profile">
+                            <img src={profileImage} alt="serfgjsrig"/>
+                        </div>
                     </div>
                 </div>
             </header>
+
+
+            <section id="street">
+                <div className="infoBoxVideo">
+                    <div className="contentInfoBoxVideo">
+                        <FaPlay />
+                        <span>Videos</span>
+                    </div>
+                </div>
+
+                <div className="boxVideo">
+                    <h3>Trilha: <span>Começando com o ensino remoto</span></h3>
+                    <div className="videos-started">
+                        <div className="gridVideos">
+
+                            <div className="box-video">
+                                <div className="image-video woman-teacher"></div>
+                                <div className="legendVideo">dawndiaewfheuhfou</div>
+                            </div>
+                            <div className="box-video">
+                                <div className="image-video"></div>
+                                <div className="legendVideo"></div>
+                            </div>
+                            <div className="box-video">
+                                <div className="image-video"></div>
+                                <div className="legendVideo"></div>
+                            </div>
+                            <div className="box-video">
+                                <div className="image-video"></div>
+                                <div className="legendVideo"></div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
